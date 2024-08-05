17b396dc3923fdfac85d745976a890d27e0f2232
2ilga <2ilgan1601@gmail.com>
2024-08-05 22:49:53 +0300

    update methods create and update in BorrowingViewSet

--- library/views.py
+++ library/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets
-
+from rest_framework.response import Response
+from rest_framework.status import *
 from library.models import *
 from library.serializers import BookSerializer, BorrowingSerializer, PaymentSerializer
 
@@ -12,6 +13,39 @@ class BookViewSet(viewsets.ModelViewSet):
 class BorrowingViewSet(viewsets.ModelViewSet):
     queryset = Borrowing.objects.all()
     serializer_class = BorrowingSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+
+        book_title = serializer.validated_data["book"]
+        book = Book.objects.get(title=book_title)
+        if book.inventory <= 0:
+            return Response({"error": "The book is not available"}, status=400)
+        book.inventory -= 1
+        book.save()
+
+        return Response(serializer.data, status=HTTP_201_CREATED, headers=headers)
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop("partial", False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, "_prefetched_objects_cache", None):
+            instance._prefetched_objects_cache = {}
+
+        if serializer.validated_data.get("actual_return_date"):
+            book = instance.book
+            book.inventory += 1
+            book.save()
+
+        return Response(serializer.data)
+
 
 
 class PaymentViewSet(viewsets.ModelViewSet):

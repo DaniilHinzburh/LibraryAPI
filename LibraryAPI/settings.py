6a0fc098f9b5985be25e8c1d26eadcc7fa2a3977
2ilga <2ilgan1601@gmail.com>
2024-08-05 17:02:17 +0300

    add serializers: BookSerializer, BorrowingSerializer, PaymentSerializer, UserSerializer, AuthTokenSerializer

--- LibraryAPI/settings.py
+++ LibraryAPI/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -76,7 +77,6 @@ REST_FRAMEWORK = {
     ],
 }
 
-
 WSGI_APPLICATION = 'LibraryAPI.wsgi.application'
 
 # Database
@@ -129,3 +129,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    "ROTATE_REFRESH_TOKENS": True
+}
